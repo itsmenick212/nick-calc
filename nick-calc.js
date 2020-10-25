5aa2f9b5ca9d3c5c368747860f7abb65d4269913
Nick Gupta <manhattan.nick@gmail.com>
2020-10-25 15:02:15 -0400

    added nick-calc.js

---
+++ nick-calc.js
@@ -0,0 +1,46 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+
+var indexRouter = require('./routes/index');
+var app = express();
+var http = require('http').createServer(app); 
+var io = require('socket.io')(http);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+var computationCache = [];
+var calculator = require('./calc.js')(io, computationCache);
+
+// Add cached calculations to every page render response
+app.use('/', function(req, res, next) {
+  res.cache = computationCache;
+  next();
+});
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+http.listen(process.env.PORT || 5000, () => {
+  console.log('listening on *:' + process.env.PORT || 5000);
+});
