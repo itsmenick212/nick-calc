523e87a9b16a7710d65357104fe35127e0e94b56
Nick Gupta <manhattan.nick@gmail.com>
2020-10-25 16:53:23 -0400

    debugged index.js in routes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Nick's Real Time Calculator', cache: res.cache });
+  res.render('index', { title: 'Nicks Real Time Calculator', cache: res.cache });
 });
 
 module.exports = router;
