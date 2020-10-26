981ab56b7892e37eb90b4ca9453c4b67eabb49a4
Nick Gupta <manhattan.nick@gmail.com>
2020-10-26 06:46:32 -0400

    modified

--- public/javascripts/calcClient.js
+++ public/javascripts/calcClient.js
@@ -1,5 +1,6 @@
 $(function () {
     var socket = io();
+
     $('form').submit(function (e) {
         e.preventDefault(); // prevents page reloading
         var eqRegex = /(\d*\.?\d+)(([+\-/*%]|\*\*)(\d*\.?\d+))+/;
@@ -7,7 +8,7 @@ $(function () {
 
         if (input === '' || input.search(eqRegex) < 0) {
             $('#user_input').val('');
-            $('#user_input').attr('placeholder', 'Input a valid calculation');
+            $('#user_input').attr('placeholder', 'INVALID ENTRY: Enter a valid entry with numbers and basic arithmetic operators');
         } else {
             socket.emit('calculation_input', $('#user_input').val());
             $('#user_input').val('');
@@ -17,14 +18,16 @@ $(function () {
     });
 
     socket.on('calculation_output', function (calc) {
-        if ($('tbody:first > tr').length > 9) {
+        if ($('tbody:first > tr').length >= 10) {
             $('tbody:first > tr:first').remove();
         }
         
+	// $("#compositeTable").find("tr:gt(-10)").remove();	
+	
         $('#calculations_output_table')
             .prepend($('<tr class="row">')
                 .append($('<td class="col">').text(calc.calc))
                 .append($('<td class="col-1">').text('='))
-                .append($('<td class="col-3">').text(calc.answer)));
+                .append($('<td class="col-2">').text(calc.answer)));
             });
 });
